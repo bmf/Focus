ed180fba4eed1b886e2a5cc222a8492ceffcf7b1
Brandon Flade <flade@falconwood.biz>
2016-08-30 13:18:20 -0400

    added .cmd file and updated PATH to location for easy execution

---
+++ focus.cmd
@@ -0,0 +1,1 @@
+@node C:\Users\brandon.flade\Documents\source\work_assist\Focus\focus.js %1
--- focus.js
+++ focus.js
@@ -1,27 +1,30 @@
 var fs = require('fs');
 
-var fileRename = ['hosts','hosts.bak'];
+var fileRename = ['hosts', 'hosts.bak'];
 
-var duration = process.argv[2] *60 *1000;
+var duration = process.argv[2] * 60 * 1000;
 
 
-fs.stat("C:\\Windows\\System32\\drivers\\etc\\" + fileRename[1], function(err,stat){
-    if(err == null) {
+fs.stat("C:\\Windows\\System32\\drivers\\etc\\" + fileRename[1], function (err, stat) {
+    if (err == null) {
         nameBack();
-        console.log("Time to get to work!");
+        console.log("\nTime to get to work!");
         setTimeout(rename, duration);
-    } else if(err.code == 'ENOENT') {
-        
-        console.log("Time to get to work!");
+    } else if (err.code == 'ENOENT') {
+
+        console.log("\nTime to get to work!");
         setTimeout(rename, duration);
-        
+
     } else {
         console.log('Some other error: ', err.code);
     }
 })
 
 
-
+function done() {
+    console.log('•••••');
+    console.log("Times up. Take a break.");
+}
 
 
 
@@ -30,22 +33,24 @@ fs.stat("C:\\Windows\\System32\\drivers\\etc\\" + fileRename[1], function(err,st
 function rename() {
 
     fs.rename("C:\\Windows\\System32\\drivers\\etc\\" + fileRename[0],
-              "C:\\Windows\\System32\\drivers\\etc\\" + fileRename[1],
-              function (err) {
-                if (err) throw err;
-              }
+        "C:\\Windows\\System32\\drivers\\etc\\" + fileRename[1],
+        function (err) {
+            if (err) throw err;
+        }
     );
+
+    done();
 }
 
 
 function nameBack() {
 
     fs.rename("C:\\Windows\\System32\\drivers\\etc\\" + fileRename[1],
-              "C:\\Windows\\System32\\drivers\\etc\\" + fileRename[0],
-              function (err) {
-                if (err) throw err;
-              }
+        "C:\\Windows\\System32\\drivers\\etc\\" + fileRename[0],
+        function (err) {
+            if (err) throw err;
+        }
     );
-    
-    
+
+
 }
